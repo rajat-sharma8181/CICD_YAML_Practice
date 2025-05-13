8018df6ef79e8c07d61dace322a2f870ebe7d902
rajat-sharma8181 <rajats12120@gmail.com>
2025-05-13 13:55:00 +0530

    variable added

--- main.tf
+++ main.tf
@@ -14,7 +14,8 @@ provider "azurerm" {
   subscription_id = "be8c30ad-e8da-4b18-bdac-c4b755dd2d80"
 }
 
+
 resource "azurerm_resorce_group" "rg1" {
-    name = "rg_preprod"
-    location = "east us"
+    name = var.rg_name
+    location = var.rg_location
 }
---
+++ variable.tf
@@ -0,0 +1,6 @@
+variable "rg_name" {
+    default = "rg_prod"
+}
+variable "rg_location" {
+    default = "east us"
+}
