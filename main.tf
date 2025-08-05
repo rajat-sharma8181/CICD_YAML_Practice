0b1952582230eeb93f7e5afb0f8ee85f5b158364
rajat-sharma8181 <164997059+rajat-sharma8181@users.noreply.github.com>
2025-08-05 16:33:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
   subscription_id = "5e4fafbb-ef69-4e17-8835-68030d81f758"
 }
 
-resourece "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg" {
 name = "CICD_rg"
 location = "central india"
 }
