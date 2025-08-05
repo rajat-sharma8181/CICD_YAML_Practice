f1c2c79354c9fc9b4d5ec0675cafc167c9a3ca5d
rajat-sharma8181 <164997059+rajat-sharma8181@users.noreply.github.com>
2025-08-05 14:53:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,17 @@ terraform {
     required_providers {
       azurerm = {
         source = "hashicorp/azurerm"
-        version = "4.28.0"
+        version = "~> 3.0"
       }
     }
 }
 
 provider "azurerm" {
-  features {
-    
-  }
-  subscription_id = "be8c30ad-e8da-4b18-bdac-c4b755dd2d80"
+  features { }
+  subscription_id = "5e4fafbb-ef69-4e17-8835-68030d81f758"
+}
+
+resourece "azurerm_resource_group" "rg" {
+name = "CICD_rg"
+location = "central india"
 }
